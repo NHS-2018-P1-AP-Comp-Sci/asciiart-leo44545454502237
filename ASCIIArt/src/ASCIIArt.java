7c6bc481ec915f21c4ce8b73a8dc4309138d796f
s-xiangs <s-xiangs@53S50029644.it.bsd405.org>
2018-09-20 08:24:17 -0700

    ASCII art project

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -15,3 +15,74 @@
  */
 
 // YOUR CODE GOES HERE :)
+
+public class ASCIIArt 
+{
+	public static int x = 0;
+	public static void DrawTopHalf()
+	{
+		System.out.println("       _______       \n    __/       \\\\__     \n __/   OOOOOOOO   \\\\__ \n/   OOOOOOOOOOOOOO    \\\\");
+
+	}
+	public static void DrawBottomHalf()
+	{
+		System.out.println("\\   OOOOOOOOOOOOOO    /");
+		System.out.println(" \\__   OOOOOOOO    __/ ");
+		System.out.println("   \\______________/    ");
+		System.out.println("      \\________/      ");		
+	}
+	public static void DrawBeam()
+	{
+		System.out.println("        |    |          ");
+	}
+	public static void DrawCow()
+	{
+
+		System.out.println("   _____/     \\_____   ");
+		System.out.println("  /  ^__^           \\  ");
+		System.out.println("  |  /oo\\           |   ");
+		System.out.println("  |  \\__/ _______   |   ");
+		System.out.println("  |      / o .   \\  |   ");
+		System.out.println("  |      | .  o .|  |   ");
+		System.out.println("  |      \\_______/  |   ");
+		System.out.println("  |       /      \\  |   ");
+		System.out.println("  \\_________________/   ");
+		
+	}
+	
+	public static void DrawCircles()
+	{
+		System.out.println("                             ");
+		System.out.println("       ____________       ");
+		System.out.println("      /__     __   \\          ");
+		System.out.println("     //  \\   /  \\   \\         ");
+		System.out.println("    / \\__/___\\__/    \\        ");
+		System.out.println("   /  /         \\   __\\           ");
+		System.out.println("  /  /_____      \\ /  \\\\            ");
+		System.out.println(" /  //__   \\    __\\\\__/ \\          ");
+		System.out.println("/  ///  \\   \\  /  \\\\     \\      ");
+		System.out.println("\\  \\\\\\__/   /  \\__//__   /         ");
+		System.out.println(" \\  \\\\_____/      //  \\ /           ");
+		System.out.println("  \\  \\           / \\__//        ");
+		System.out.println("   \\  \\_________/     /        ");
+		System.out.println("    \\ /  \\   /  \\    /         ");
+		System.out.println("     \\\\__/   \\__/   /            ");
+		System.out.println("      \\____________/       ");
+		System.out.println("                            ");
+
+
+	}
+	public static void main(String[] args)
+	{
+		
+		DrawTopHalf();
+		DrawBottomHalf();
+		while (x<10)
+		{
+			DrawBeam();
+			x+=1;
+		}
+		DrawCow();
+		DrawCircles();
+	}
+}
